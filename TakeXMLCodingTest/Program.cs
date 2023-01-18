c6da0077c8ba30df13c844f0ff4ee4dc59e148ee
tpatoz <tpatoz@ARC110NB>
2023-01-18 09:09:53 +0900

    Add project files.

---
+++ TakeXMLCodingTest/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Xml;
+
+namespace TakeXMLCodingTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Abbas Toz");
+            Console.WriteLine("Take home coding test (XML)");   
+            string example= "<tutorial date='01 / 01 / 2000'>XML</tutorial>";
+            string example2 = "<tutorial date>XML</tutorial date>";
+
+            string exampleFirst = "<Design><Code>hello world</Code></Design>";
+			string exampleSecond = "<Design><Code>hello world</Code></Design><People>";
+			string exampleThird = "<People><Design><Code>hello world</People></Code></Design>";
+			string exampleFourth = "<People age=”1”>hello world</People>";
+
+			Console.WriteLine("Example  :" + DetermineXml(example));
+			Console.WriteLine("Example2 :" + DetermineXml(example2));
+			Console.WriteLine("Example 1:" + DetermineXml(exampleFirst));
+			Console.WriteLine("Example 2:" + DetermineXml(exampleSecond));
+			Console.WriteLine("Example 3:" + DetermineXml(exampleThird));
+			Console.WriteLine("Example 4:" + DetermineXml(exampleFourth));
+			Console.ReadKey();
+        }
+        public static bool DetermineXml(string xml)
+        {
+            var count = 0; //tag count
+            var stack = new Stack<Tag>();            
+           
+            for (var i = 0; i < xml.Length; i++)
+            {
+                if (xml[i] == ' ') continue;
+              
+                    if (xml[i] == '<')
+                    {
+                        var next = xml.IndexOf('>', i + 1);
+                        if (next <= 0) return false;
+
+                        var name = xml.Substring(i + 1, next - (i + 1));
+                        if (name.Length < 1) return false;
+                        if (xml[i + 1] == '/')
+                        {
+                            name = name.Substring(1);
+                            //end tag
+
+                            if (stack.Count == 0) return false;
+                            var tag = stack.Pop();
+                            if (!tag.TagName.Equals(name)) return false;
+                        }
+                        else
+                        {
+                            if (count > 0 && stack.Count == 0) return false;
+
+                            //start tag
+                            var tag = new Tag()
+                            {
+                                TagName = name,
+                            };
+                            stack.Push(tag);
+                            count++;
+                        }
+                       
+                        i = next;
+                    }
+                    else if (count == 0)
+                    {
+                        return false;
+                    }
+                    else if (stack.Count == 0)
+                    {
+                        return false;
+                    }
+                }
+            
+            return stack.Count == 0;
+        }
+
+
+        
+        public class Tag
+        {
+            public string TagName;           
+        }
+    }
+}
